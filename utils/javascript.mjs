89ae47c0a3c07ea6ea70f76210dbd9cadbb4be82
Necaladun <mlazauskas@gmail.com>
2026-01-16 04:14:35 +1030

    Revert "Stage release in build/release and redirect build outputs"

--- utils/javascript.mjs
+++ utils/javascript.mjs
@@ -1,13 +1,11 @@
 /** Modified copy of file found in dnd5e (https://github.com/foundryvtt/dnd5e/blob/master/LICENSE.txt) */
 
 import eslint from "gulp-eslint-new";
-import fs from "fs";
 import gulp from "gulp";
 import gulpIf from "gulp-if";
 import mergeStream from "merge-stream";
-import path from "path";
 import yargs from "yargs";
-import { rollup } from "rollup";
+import { rollup } from 'rollup';
 
 
 /**
@@ -45,15 +43,13 @@ function lintJavascript() {
 export const lint = lintJavascript;
 
 function bundleJavascript() {
-  const outputPath = "./build/release/script/dark-heresy.js";
-  fs.mkdirSync(path.dirname(outputPath), { recursive: true });
-  return rollup({
-      input: "./script/dark-heresy.js",
-    })
-    .then(bundle => {
-      return bundle.write({
-        file: outputPath
-      });
-    });
+	return rollup({
+			input: './script/dark-heresy.js',
+		})
+		.then(bundle => {
+			return bundle.write({
+				file: './release/script/dark-heresy.js'
+			});
+		});
 }
 export const bundle = bundleJavascript; 
